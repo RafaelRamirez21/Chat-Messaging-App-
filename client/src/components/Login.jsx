f3762e7d9c72a82d445ced54ffe7a3ee7b66179d
Rafael Ramírez <rrramirezb251@gmail.com>
2021-10-22 09:29:52 -0500

    channels added

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -3,11 +3,14 @@ import '../assets/styles/login.scss'
 
 const Login = () => {
   return (
+
     <div>
+
       <div className="top"></div>
       <div className="bottom"></div>
     </div>
   )
 }
 
-export default Login
+export default Login;
+
--- client/src/routes/App.jsx
+++ client/src/routes/App.jsx
@@ -1,22 +1,28 @@
 import React from 'react';
 import Home from '../components/Home';
 import Login from '../components/Login';
+// import MainViewChat from '../components/chats/MainViewChat';
 import {
   BrowserRouter as Router,
   Route,
   Switch
 } from "react-router-dom";
+import ViewMain from '../components/ViewChats/ViewMain';
+
 
 
 const App = () => {
 
-  return( 
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/login" component={Login} />
-      </Switch>
-    </Router>
+  return (
+    <>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/chat" component={ViewMain} />
+        </Switch>
+      </Router>
+    </>
   )
 }
 
