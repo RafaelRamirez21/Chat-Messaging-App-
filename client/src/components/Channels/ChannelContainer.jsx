c8c3e0c9b3167c22d1c52014b6b89d9fbcbc4d60
Rafael Ramírez <rrramirezb251@gmail.com>
2021-10-22 09:30:19 -0500

    updated components

---
+++ client/src/components/Channels/ChannelContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ChannelContainer = () => {
+  return (
+    <div>
+      Channel container
+    </div>
+  )
+}
+
+export default ChannelContainer;
---
+++ client/src/components/ViewChats/ViewMain.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StreamChat } from 'stream-chat';
+import { Chat } from 'stream-chat-react';
+import cookies from 'universal-cookie';
+import { ChannelListContainer, ChannelContainer } from '../../components';
+
+const apiKey = 'mw4x9ttkdtcf';
+const client = StreamChat.getInstance(apiKey);
+const ViewMain = () => {
+  return (
+    <>
+      <div className='main-view-chat'>
+        <Chat client={client} theme='team light'>
+          <ChanelListContainer>
+
+          </ChanelListContainer>
+
+        </Chat>
+      </div>
+
+
+    </>
+  )
+}
+
+export default ViewMain
