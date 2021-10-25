23c7dd720ac0a66d6d9d86cef7f917e2c97eb56f
Sebastian Ocampo <sebastianocampolopez507@gmail.com>
2021-10-25 15:35:56 -0500

    CreateChannel was updated

--- client/src/assets/CloseCreateChannel.js
+++ client/src/assets/CloseCreateChannel.js
@@ -4,6 +4,7 @@ export const CloseCreateChannel = ({ setIsCreating, setIsEditing }) => (
   <svg
     width='32'
     height='32'
+    cursor='pointer'
     viewBox='0 0 32 32'
     fill='none'
     xmlns='http://www.w3.org/2000/svg'
